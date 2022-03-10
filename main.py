292cf8489a1d018d8d98db138ecde5e09ff12929
Stefan Popov <popovstefan@live.com>
2022-03-10 22:43:32 +0100

    Passed tests

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import json
-
-import mlflow
-import tempfile
 import os
-import wandb
+import tempfile
+
 import hydra
+import mlflow
 from omegaconf import DictConfig
 
 _steps = [
@@ -16,14 +15,13 @@ _steps = [
     # NOTE: We do not include this in the steps so it is not run by mistake.
     # You first need to promote a model export to "prod" before you can run this,
     # then you need to run this step explicitly
-#    "test_regression_model"
+    #    "test_regression_model"
 ]
 
 
 # This automatically reads in the configuration
 @hydra.main(config_name='config')
 def go(config: DictConfig):
-
     # Setup the wandb experiment. All runs will be grouped under this name
     os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
     os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
@@ -42,9 +40,9 @@ def go(config: DictConfig):
                 "main",
                 parameters={
                     "sample": config["etl"]["sample"],
-                    "artifact_name": "sample.csv",
-                    "artifact_type": "raw_data",
-                    "artifact_description": "Raw file as downloaded"
+                    "artifact_name": config['download']['artifact_name'],
+                    "artifact_type": config['download']['artifact_type'],
+                    "artifact_description": config['download']['artifact_description']
                 },
             )
 
@@ -63,10 +61,17 @@ def go(config: DictConfig):
             )
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+                "main",
+                parameters={
+                    "csv": config['data_check']['csv'],
+                    "ref": config['data_check']['ref'],
+                    "kl_threshold": config['data_check']['kl_threshold'],
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
+                },
+            )
 
         if "data_split" in active_steps:
             ##################
@@ -75,7 +80,6 @@ def go(config: DictConfig):
             pass
 
         if "train_random_forest" in active_steps:
-
             # NOTE: we need to serialize the random forest configuration into JSON
             rf_config = os.path.abspath("rf_config.json")
             with open(rf_config, "w+") as fp:
@@ -91,7 +95,6 @@ def go(config: DictConfig):
             pass
 
         if "test_regression_model" in active_steps:
-
             ##################
             # Implement here #
             ##################
--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,6 +60,11 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data: pd.DataFrame):
+    """Check that the row size is reasonable (that there are tens of thousands of rows)."""
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """Verify the range of renting prices."""
+    assert data['price'].between(min_price, max_price).all()
