9d486dd7f74ed4a58e2ef036b707293023ca5b13
Stefan Popov <popovstefan@live.com>
2022-03-11 11:40:54 +0100

    Fix geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -31,6 +31,9 @@ def go(args):
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
     logger.info(f"Saving output artifact with shape {df.shape}")
+    # Geolocation
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
     # Save to csv
     df.to_csv(args.output_artifact, index=False)
     # Upload to W&B
